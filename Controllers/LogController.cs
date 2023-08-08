4066d149a2f3bca4f6263467c148ca3e1d1db4fc
Ed Mendoza <luisemendoza3@outlook.com>
2023-08-08 13:45:22 -0600

    First Commit

---
+++ Controllers/LogController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace IotCoreDemo.Controllers
+{
+    [Route("api/v1/")]
+    [ApiController]
+    [Produces("application/json")]
+    public class LogController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public LogController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [Route("/logs")]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var logList = await _unitOfWork.Logs.GetAllAsync();
+            return Ok(logList);
+        }
+    }
+}
---
+++ Requests/CommandRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace IotCoreDemo.Requests
+{
+    public class CommandRequest
+    {
+        public int commad { get; set; }
+        public string user_uuid { get; set; } = default!;
+    }
+}
